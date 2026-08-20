4f675fe8048b52f09825aef73bc09bd956e652ef
cipheroot <cipheroot@proton.me>
2026-08-20 21:08:27 +0530

    Fix Vercel build hang and ESLint checks

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -5,6 +5,6 @@ import * as schema from "./schema"
 const connectionString = process.env.DATABASE_URL!
 
 // Disable prefetch for Supabase's transaction mode pooler
-const client = postgres(connectionString, { prepare: false })
+const client = postgres(connectionString, { prepare: false, idle_timeout: 5 })
 
 export const db = drizzle(client, { schema })
